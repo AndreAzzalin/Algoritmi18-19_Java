ff84c05fc06074e3060791f01b985ad0a37dc3c5
Andrea <andreazzalin@gmail.com>
2019-12-06 18:29:53 +0100

    complete es2 init es3

--- src/com/eserciziJava/esercizio2/CorrectMe.java
+++ src/com/eserciziJava/esercizio2/CorrectMe.java
@@ -1,22 +1,18 @@
 package com.eserciziJava.esercizio2;
 
-import javafx.util.Pair;
-
+import org.jetbrains.annotations.NotNull;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-
 import static java.util.function.Predicate.not;
 
 public class CorrectMe {
 
 
-    public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 
 
 /*
@@ -26,114 +22,115 @@ public class CorrectMe {
     il correttore non deve piú calcolare l'edit distance quando la lunghezza della parola del dizionario
     risulta maggiore dell'edit distance minima.
  */
-        String basePath = System.getProperty("user.dir") + getOS()+"src" + getOS() + "com" + getOS() + "eserciziJava" + getOS() + "esercizio2" + getOS() + "datasets" + getOS();
-        String pathToCorrectMe = basePath + "correctme.txt";
-        String pathToDictionary = basePath + "dictionary.txt";
+		String basePath = System.getProperty("user.dir") + getOS() + "src" + getOS() + "com" + getOS() + "eserciziJava" + getOS() + "esercizio2" + getOS() + "datasets" + getOS();
+		String pathToCorrectMe = basePath + "correctme.txt";
+		String pathToDictionary = basePath + "dictionary.txt";
 
 
-        System.out.println("Default path for datasets:");
-        System.out.println("Path for correctme.txt -> " + pathToCorrectMe);
-        System.out.println("Path for dictionary.txt -> " + pathToDictionary);
+		System.out.println("Default path for datasets:");
+		System.out.println("Path for correctme.txt -> " + pathToCorrectMe);
+		System.out.println("Path for dictionary.txt -> " + pathToDictionary);
 
-        Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 
-        System.out.println("Do you want change dafault path? Y / N ");
-        String choice = sc.nextLine();
-        if (choice.equals("Y")){
-            System.out.println("Insert new correctme.txt path:");
-            pathToCorrectMe = sc.nextLine();
-            System.out.println("Insert new dictionary.txt path:");
-            pathToDictionary = sc.nextLine();
-        }
-
-
-        assumeCorrections(pathToCorrectMe, pathToDictionary);
-
-    }
+		System.out.println("Do you want change dafault path? Y / N ");
+		String choice = sc.nextLine();
+		if (choice.equals("Y")||choice.equals("y")) {
+			System.out.println("Insert new correctme.txt path:");
+			pathToCorrectMe = sc.nextLine();
+			System.out.println("Insert new dictionary.txt path:");
+			pathToDictionary = sc.nextLine();
+		}
 
 
-    private static void assumeCorrections(String pathToCorrectMe, String pathToDictionary) {
-        List<String> correctMe = getCorrectMeList(pathToCorrectMe);
-        List<String> dictionary = getDictionaryList(pathToDictionary);
-        List<String> possibleStrings = new ArrayList<>();
+		assumeCorrections(pathToCorrectMe, pathToDictionary);
 
-        long startTime = System.currentTimeMillis();
-        int editDistMin = Integer.MAX_VALUE;
-        int editDistRes = 0;
+	}
 
 
-        for (String toCorrectString : correctMe) {
-            for (String dicString : dictionary) {
-                if (dicString.length() - toCorrectString.length() > editDistMin) {
-                    break;
-                }
+	private static void assumeCorrections(String pathToCorrectMe, String pathToDictionary) {
+		List<String> correctMe = getCorrectMeList(pathToCorrectMe);
+		List<String> dictionary = getDictionaryList(pathToDictionary);
+		List<String> possibleStrings = new ArrayList<>();
 
-                editDistRes = EditDistance.calculateDyn(toCorrectString, dicString);
-
-                if (editDistRes < editDistMin) {
-                    editDistMin = editDistRes;
-                    possibleStrings.clear();
-                    possibleStrings.add(dicString);
-                } else if (editDistMin == editDistRes) {
-                    possibleStrings.add(dicString);
-                }
-            }
-
-            System.out.println("\nWord: '" + toCorrectString + " Edit distance: " + editDistMin);
-            possibleStrings.forEach(item -> System.out.print(" | " + item));
-
-            //resetto variabili
-            possibleStrings.clear();
-            editDistMin = Integer.MAX_VALUE;
-        }
+		long startTime = System.currentTimeMillis();
+		int editDistMin = Integer.MAX_VALUE;
+		int editDistRes = 0;
 
 
-        long endTime = System.currentTimeMillis();
-        System.out.println("\nExecution time: " + ((endTime - startTime) / 1000) + " s\n");
-    }
+		for (String toCorrectString : correctMe) {
+			for (String dicString : dictionary) {
+				if (dicString.length() - toCorrectString.length() > editDistMin) {
+					break;
+				}
 
-    private static List<String> getCorrectMeList(String path) {
-        List<String> correctMeList = new ArrayList<>();
+				editDistRes = EditDistance.calculateDyn(toCorrectString, dicString);
 
-        try (Stream<String> correctMeFileStream = Files.lines(Paths.get(path))) {
-            //dallo stream del file filtra gli spazi e inserisci il contenuto in una lista
-            correctMeList = correctMeFileStream
-                    .flatMap(str -> Arrays.stream(str.split("\\W"))
-                            .map(String::toLowerCase)
-                            .filter(not(items -> items.equals(""))))
-                    .collect(Collectors.toList());
-        } catch (IOException e) {
-            System.out.println("Error: " + e + " | Insert valid .txt in datasets");
-        }
+				if (editDistRes < editDistMin) {
+					editDistMin = editDistRes;
+					possibleStrings.clear();
+					possibleStrings.add(dicString);
+				} else if (editDistMin == editDistRes) {
+					possibleStrings.add(dicString);
+				}
+			}
 
-        return correctMeList;
-    }
+			System.out.println("\nWord: " + toCorrectString + " Edit distance: " + editDistMin);
+			possibleStrings.forEach(item -> System.out.print(" | " + item));
 
-    private static List<String> getDictionaryList(String path) {
-        List<String> dictionaryList = new ArrayList<>();
-
-        try (Stream<String> dictionaryFileStream = Files.lines(Paths.get(path))) {
-            Comparator<String> sortByLength = Comparator.comparingInt(String::length);
-            dictionaryList = dictionaryFileStream
-                    .sorted(sortByLength)
-                    .collect(Collectors.toList());
-        } catch (IOException e) {
-            System.out.println("Error: " + e + " | Insert valid .txt in datasets");
-        }
-
-        return dictionaryList;
-    }
-
-    private static String getOS() {
-        String os = System.getProperty("os.name");
-
-        if (os.contains("Windows"))
-            return "\\";
-        else
-            return "//";
+			//resetto variabili
+			possibleStrings.clear();
+			editDistMin = Integer.MAX_VALUE;
+		}
 
 
-    }
+		long endTime = System.currentTimeMillis();
+		System.out.println("\n\nExecution time: " + ((endTime - startTime) / 1000) + "s");
+	}
+
+	private static List<String> getCorrectMeList(String path) {
+		List<String> correctMeList = new ArrayList<>();
+
+		try (Stream<String> correctMeFileStream = Files.lines(Paths.get(path))) {
+			//dallo stream del file filtra gli spazi e inserisci il contenuto in una lista
+			correctMeList = correctMeFileStream
+							.flatMap(str -> Arrays.stream(str.split("\\W"))
+											.map(String::toLowerCase)
+											.filter(not(items -> items.equals(""))))
+							.collect(Collectors.toList());
+		} catch (IOException e) {
+			System.out.println("Error: " + e + " | Insert valid .txt in datasets");
+		}
+
+		return correctMeList;
+	}
+
+	private static List<String> getDictionaryList(String path) {
+		List<String> dictionaryList = new ArrayList<>();
+
+		try (Stream<String> dictionaryFileStream = Files.lines(Paths.get(path))) {
+			Comparator<String> sortByLength = Comparator.comparingInt(String::length);
+			dictionaryList = dictionaryFileStream
+							.sorted(sortByLength)
+							.collect(Collectors.toList());
+		} catch (IOException e) {
+			System.out.println("Error: " + e + " | Insert valid .txt in datasets");
+		}
+
+		return dictionaryList;
+	}
+
+	@NotNull
+	private static String getOS() {
+		String os = System.getProperty("os.name");
+
+		if (os.contains("Windows"))
+			return "\\";
+		else
+			return "//";
+
+
+	}
 
 }
 
--- src/com/eserciziJava/esercizio2/EditDistance.java
+++ src/com/eserciziJava/esercizio2/EditDistance.java
@@ -5,10 +5,9 @@ import javafx.util.Pair;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Map;
+
 
 public class EditDistance {
 
@@ -18,73 +17,73 @@ public class EditDistance {
     https://dzone.com/articles/java-8-automatic-memoization
     */
 
-    //senza l'annotation ritorn null pointer exception, così facendo sappiamo quale parametro è stato impostato a null
-    public static int calculate(@NotNull String s1, @NotNull String s2) {
+	//senza l'annotation ritorn null pointer exception, così facendo sappiamo quale parametro è stato impostato a null
+	public static int calculate(@NotNull String s1, @NotNull String s2) {
 
-        if (s1.isEmpty()) {
-            return s2.length();
-        }
+		if (s1.isEmpty()) {
+			return s2.length();
+		}
 
-        if (s2.isEmpty()) {
-            return s1.length();
-        }
+		if (s2.isEmpty()) {
+			return s1.length();
+		}
 
-        //slide 18
-        int replace = calculate(s1.substring(1), s2.substring(1)) + costOfSubstitution(s1.charAt(0), s2.charAt(0));
-        int insert = calculate(s1, s2.substring(1)) + 1;
-        int remove = calculate(s1.substring(1), s2) + 1;
+		//slide 18
+		int replace = calculate(s1.substring(1), s2.substring(1)) + costOfSubstitution(s1.charAt(0), s2.charAt(0));
+		int insert = calculate(s1, s2.substring(1)) + 1;
+		int remove = calculate(s1.substring(1), s2) + 1;
 
-        return min(replace, insert, remove);
-    }
+		return min(replace, insert, remove);
+	}
 
-    public static int calculateDyn(@NotNull String s1, @NotNull String s2) {
-        HashMap<Pair<String, String>, Integer> problemSolved = new HashMap<>();
-        return calculateDynRecursive(s1, s2, problemSolved);
-    }
+	public static int calculateDyn(@NotNull String s1, @NotNull String s2) {
+		HashMap<Pair<String, String>, Integer> problemSolved = new HashMap<>();
+		return calculateDynRecursive(s1, s2, problemSolved);
+	}
 
-    private static int calculateDynRecursive(String s1, String s2, HashMap<Pair<String, String>, Integer> problemSolved) {
-        int no_op, min;
+	private static int calculateDynRecursive(String s1, String s2, HashMap<Pair<String, String>, Integer> problemSolved) {
+		int no_op, min;
 
-        if (problemSolved.containsKey(new Pair<>(s1, s2)))
-            return problemSolved.get(new Pair<>(s1, s2));
+		if (problemSolved.containsKey(new Pair<>(s1, s2)))
+			return problemSolved.get(new Pair<>(s1, s2));
 
-        if (s1.length() == 0) {
-            problemSolved.put(new Pair<>(s1, s2), s2.length());
-            return s2.length();
-        }
+		if (s1.length() == 0) {
+			problemSolved.put(new Pair<>(s1, s2), s2.length());
+			return s2.length();
+		}
 
-        if (s2.length() == 0) {
-            problemSolved.put(new Pair<>(s1, s2), s1.length());
-            return s1.length();
-        }
+		if (s2.length() == 0) {
+			problemSolved.put(new Pair<>(s1, s2), s1.length());
+			return s1.length();
+		}
 
-        if (s1.charAt(0) == s2.charAt(0))
-            no_op = calculateDynRecursive(s1.substring(1), s2.substring(1), problemSolved);
-        else no_op = -1;
+		if (s1.charAt(0) == s2.charAt(0))
+			no_op = calculateDynRecursive(s1.substring(1), s2.substring(1), problemSolved);
+		else no_op = -1;
 
-        int remove = 1 + calculateDynRecursive(s1, s2.substring(1), problemSolved);
-        int insert = 1 + calculateDynRecursive(s1.substring(1), s2, problemSolved);
-        int replace = calculateDynRecursive(s1.substring(1), s2.substring(1),problemSolved) + costOfSubstitution(s1.charAt(0), s2.charAt(0));
+		int remove = 1 + calculateDynRecursive(s1, s2.substring(1), problemSolved);
+		int insert = 1 + calculateDynRecursive(s1.substring(1), s2, problemSolved);
+		int replace = calculateDynRecursive(s1.substring(1), s2.substring(1), problemSolved) + costOfSubstitution(s1.charAt(0), s2.charAt(0));
 
-        if (no_op == -1) {
-            problemSolved.put(new Pair<>(s1, s2), min(remove, insert));
-            return min(remove, insert,replace);
-        } else
-            min = min(remove, insert, no_op,replace);
-        problemSolved.put(new Pair<>(s1, s2), min);
+		if (no_op == -1) {
+			problemSolved.put(new Pair<>(s1, s2), min(remove, insert));
+			return min(remove, insert, replace);
+		} else
+			min = min(remove, insert, no_op, replace);
+		problemSolved.put(new Pair<>(s1, s2), min);
 
-        return min;
-    }
+		return min;
+	}
 
 
-    @Contract(pure = true)
-    private static int costOfSubstitution(char a, char b) {
-        return a == b ? 0 : 1;
-    }
+	@Contract(pure = true)
+	private static int costOfSubstitution(char a, char b) {
+		return a == b ? 0 : 1;
+	}
 
-    //get min from stream of numbers
-    private static int min(int... numbers) {
-        return Arrays.stream(numbers)
-                .min().orElse(Integer.MAX_VALUE);
-    }
+	//get min from stream of numbers
+	private static int min(int... numbers) {
+		return Arrays.stream(numbers)
+						.min().orElse(Integer.MAX_VALUE);
+	}
 }
